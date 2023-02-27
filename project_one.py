4fbe7a8041442d42fbd7a98a459fd4397b9fa8a6
Sobble816 <124954983+Sobble816@users.noreply.github.com>
2023-02-26 22:21:48 -0500

    Update project_one.py
    Add comment

--- project_one.py
+++ project_one.py
@@ -1,19 +1,22 @@
-# Imports go at the top
+# Imports the dependency
 from microbit import *
 import random
 
-#your position
+# your initial position at (2,4)
 x = 2
 y = 4
 
-# object position
+# random target object position 
 OBJ_X = random.randint(0,4)
 OBJ_Y = random.randint(0,4)
 
+# initialize score to zero
 score = 0
 
+# start of the infinite loop
 while True:
-    #when moving the mircrobit
+    # move microbit according the accelerometer guesture. 
+    # NOTE: (0,0) is top left corner.  Therefore, "Down" gesture means y increasing
     if accelerometer.is_gesture('left'):
         x -= 1
     if accelerometer.is_gesture('right'):
@@ -23,6 +26,7 @@ while True:
     if accelerometer.is_gesture('up'):
         y -= 1
 
+    # if user object move to boundary, it will stay there without going beyond.
     if x == -1:
         x = 0
     if x == 5:
@@ -32,21 +36,23 @@ while True:
     if y == 5:
         y = 4
 
-    #when you hit the object
+    # increase the score when you hit the object and generate a new object position
     if OBJ_X == x and OBJ_Y == y: 
         score += 1
         OBJ_X = random.randint(0,4)
         OBJ_Y = random.randint(0,4)
 
-    display.set_pixel(x, y, 9) #show your location
-    display.set_pixel(OBJ_X, OBJ_Y, 6) #show object's location
+    display.set_pixel(x, y, 9) # show your location (with brighter dot)
+    display.set_pixel(OBJ_X, OBJ_Y, 6) # show object's location (with lighter dot)
     # the value of sleep can control how fast the bit move.
     # lower the value the bit will move further
     sleep(300)
     display.clear()
     
-    if button_a.was_pressed(): #end games
+    # end games if button A is pressed
+    if button_a.was_pressed(): 
         break
-        
+
+# display the score if the game ends (breaking the infinite loop)
 display.scroll(score)
 reset()
